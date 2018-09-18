6680d3f9fc25aafc9b4d824cdf99c3221866c91c
Jóhannes Ingi Torfason <johannest18@ru.is>
2018-09-18 12:49:25 +0000

    Second commit

--- anfalla.py
+++ anfalla.py
@@ -1,6 +1,6 @@
 #Velja upphafsstaðsetningu x og y
 
-#Setja inn færsluaðgerð
+#Setja inn færsluaðgerð, þe. setja inn skipanir fyrir færslu og láta kall færast
 #Setja inn hvað gerjst gerist í hverjum af 9 tælum
 #Setja inn rettar prent skipanir og biðji um input á réttum stöðum
 #prenta út victory ef þetta lendir á réttum stað.
