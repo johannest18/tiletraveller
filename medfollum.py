cc960dfb1f9ab0f232598337d0863f8a9e4754f3
Jóhannes Ingi Torfason <johannest18@ru.is>
2018-09-18 14:40:42 +0000

    Allt í vinnslu

--- medfollum.py
+++ medfollum.py
@@ -1,8 +1,10 @@
 #Velja upphafsstaðsetningu x og y
 locationx, locationy = 1, 1
-
+location = [locationx, locationy]
+#Setja inn færsluaðgerð, þe. setja inn skipanir fyrir færslu og láta kall færast
 def faersla(s, x, y):
-    if s == valid:
+    s = s.lower()
+    if validdirection == 1:
         if s == "n":
             x+=1
         elif s == "s":
@@ -12,12 +14,35 @@ def faersla(s, x, y):
         elif s == "e":
             y +=1
     else:
-        print(Not a valid direction)            
-            
+        print("Not a valid direction!")
+    return x, y
 
-#Setja inn færsluaðgerð, þe. setja inn skipanir fyrir færslu og láta kall færast
+#Setja inn prentaðgerð
+def youcantravel(n, e, s, w):
+    message="You can travel: "
+    if n == 1:
+        message += "(N)orth"
+    if e == 1:
+        message += " or (E)ast"
+    if s == 1:
+        message += " or (S)outh"
+    if w == 1:
+        message += " or (W)est"
+    print(message+".")
+        
+victory = locationx == [3,1]
+if not victory:
+    youcantravel(1, 1, 0 ,0)
+    
+
 #Setja inn færsluaðgerð, þe. setja inn skipanir fyrir færslu og láta kall færast
 #Setja inn hvað gerjst gerist í hverjum af 9 tælum
 #Setja inn rettar prent skipanir og biðji um input á réttum stöðum
 
 #prenta út victory ef þetta lendir á réttum stað.n
+siggi = "n"
+validdirection = 1
+locationx, locationy = faersla(siggi, locationx, locationy)
+print(locationx, locationy)
+locationx, locationy = faersla(siggi, locationx, locationy)
+print(locationx, locationy)
