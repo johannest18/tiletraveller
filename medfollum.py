988c1b92e3b426c784019c6d1392bd5b5a739c64
Jóhannes Ingi Torfason <johannest18@ru.is>
2018-09-18 14:15:56 +0000

    fyrsta commit

---
+++ medfollum.py
@@ -0,0 +1,23 @@
+#Velja upphafsstaðsetningu x og y
+locationx, locationy = 1, 1
+
+def faersla(s, x, y):
+    if s == valid:
+        if s == "n":
+            x+=1
+        elif s == "s":
+            x-=1
+        elif s == "w":
+            y -=1
+        elif s == "e":
+            y +=1
+    else:
+        print(Not a valid direction)            
+            
+
+#Setja inn færsluaðgerð, þe. setja inn skipanir fyrir færslu og láta kall færast
+#Setja inn færsluaðgerð, þe. setja inn skipanir fyrir færslu og láta kall færast
+#Setja inn hvað gerjst gerist í hverjum af 9 tælum
+#Setja inn rettar prent skipanir og biðji um input á réttum stöðum
+
+#prenta út victory ef þetta lendir á réttum stað.n
