2343405ae8a576cf2df6548bfc708f2712023d98
Jóhannes Ingi Torfason <johannest18@ru.is>
2018-09-18 12:47:30 +0000

    First commit

---
+++ anfalla.py
@@ -0,0 +1,6 @@
+#Velja upphafsstaðsetningu x og y
+
+#Setja inn færsluaðgerð
+#Setja inn hvað gerjst gerist í hverjum af 9 tælum
+#Setja inn rettar prent skipanir og biðji um input á réttum stöðum
+#prenta út victory ef þetta lendir á réttum stað.
