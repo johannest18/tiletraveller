492b5e7dff40d0595d889b2bbdd6e995c14c027a
Jóhannes Ingi Torfason <johannest18@ru.is>
2018-09-19 12:54:29 +0000

    Búinn að sofa á þessu og laga föll

--- medfollum.py
+++ medfollum.py
@@ -2,20 +2,21 @@
 locationx, locationy = 1, 1
 location = [locationx, locationy]
 #Setja inn færsluaðgerð, þe. setja inn skipanir fyrir færslu og láta kall færast
-def faersla(s, x, y):
-    s = s.lower()
-    if validdirection == 1:
-        if s == "n":
-            x+=1
-        elif s == "s":
-            x-=1
-        elif s == "w":
-            y -=1
-        elif s == "e":
-            y +=1
+def faersla(t, f, x, y):
+    t = t.lower()
+    if t == "n" and n==1:
+        y+=1
+    elif t == "s" and s ==1:
+        y-=1
+    elif t == "w" and w == 1:
+        x -=1
+    elif t == "e" and e == 1:
+        x +=1
     else:
         print("Not a valid direction!")
-    return x, y
+    f = [x, y]
+    return f, x, y
+#athuga hvort eitthvað sé valid direction
 
 #Setja inn prentaðgerð
 def youcantravel(n, e, s, w):
@@ -29,20 +30,43 @@ def youcantravel(n, e, s, w):
     if w == 1:
         message += " or (W)est"
     print(message+".")
-        
-victory = locationx == [3,1]
-if not victory:
-    youcantravel(1, 1, 0 ,0)
-    
+#setja inn inputaðgerð
+def inputid():
+    faerslan = input("Direction: ")
+    faerslan = faerslan.lower()
+    return faerslan
 
+def heildarfærsla(no, ea, so, we, x, y, xy):
+        youcantravel(n, e, s, w)
+        location2 = xy
+        while location2 == xy:
+            attin = inputid()
+            xy, x, y = faersla(attin, xy, x, y)      
+        return xy, x, y 
+
+victory = [3,1]
+while location != victory:
+    if location == [1,1] or location == [2,1]:
+        n, e, s, w = 1,0,0,0
+        location, locationx, locationy = heildarfærsla(n, e, s, w, locationx, locationy, location)
+    if location == [1,2]:
+        n, e, s, w= 1, 1, 1 ,0
+        location, locationx, locationy = heildarfærsla(n, e, s, w, locationx, locationy, location)
+    if location == [2,2] or location == [3,3]:
+        n, e, s, w= 0, 0, 1 ,1
+        location, locationx, locationy = heildarfærsla(n, e, s, w, locationx, locationy, location)
+    if location == [1,3]:
+        n, e, s, w= 0, 1, 1 ,0
+        location, locationx, locationy = heildarfærsla(n, e, s, w, locationx, locationy, location)
+    if location == [2,3]:
+        n, e, s, w= 0, 1, 0 ,1
+        location, locationx, locationy = heildarfærsla(n, e, s, w, locationx, locationy, location)
+    if location == [3,2]:
+        n, e, s, w= 1, 0, 1 ,0
+        location, locationx, locationy = heildarfærsla(n, e, s, w, locationx, locationy, location)
+print("Victory!")
 #Setja inn færsluaðgerð, þe. setja inn skipanir fyrir færslu og láta kall færast
 #Setja inn hvað gerjst gerist í hverjum af 9 tælum
 #Setja inn rettar prent skipanir og biðji um input á réttum stöðum
 
-#prenta út victory ef þetta lendir á réttum stað.n
-siggi = "n"
-validdirection = 1
-locationx, locationy = faersla(siggi, locationx, locationy)
-print(locationx, locationy)
-locationx, locationy = faersla(siggi, locationx, locationy)
-print(locationx, locationy)
+#prenta út victory ef þetta lendir á réttum stað.
